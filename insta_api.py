153fbb85cebdb306b91c4438cc57b1173ccb86d2
Aleksandr Semenov <semenlinux@gmail.com>
2021-02-01 14:04:33 +0300

    Download image from link

--- insta_api.py
+++ insta_api.py
@@ -6,22 +6,23 @@ from pathvalidate import sanitize_filename, sanitize_filepath
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 
-def get_data_from_link(link:str) -> requests.Response:
+def get_data_from_link(link: str) -> requests.models.Response:
     link_response = requests.get(link, verify=False, allow_redirects=False)
     link_response.raise_for_status()
     return link_response
 
 
-def write_image_to_file(data, full_path_file: str) -> str:
+def write_image_to_file(data: bytes, full_path_file: str) -> str:
     with open(full_path_file, "wb") as file:
         file.write(data)
     return full_path_file
 
 
-def download_image(link: str, filename: str, folder:str ="./")-> str:
+def download_image(link: str, filename: str, folder: str = "./") -> str:
+    checked_folder = sanitize_filepath(folder)
     checked_filename = sanitize_filename(filename)
     Path(f"./{ folder }").mkdir(parents=True, exist_ok=True)
-    string_filepath = f"{ os.path.join(folder, checked_filename) }"
+    string_filepath = f"{ os.path.join(checked_folder, checked_filename) }"
     image_data = get_data_from_link(link)
     file_with_data_filepath = write_image_to_file(image_data.content, string_filepath)
     return file_with_data_filepath
@@ -29,12 +30,13 @@ def download_image(link: str, filename: str, folder:str ="./")-> str:
 
 def main():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    link="https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    link = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
     filename = "hubble.jpeg"
     try:
         print(download_image(link, filename))
     except (requests.ConnectionError, requests.HTTPError):
         print("Что-то пошло не так:( Проверьте подключение к интернету!")
+
 
 if __name__ == "__main__":
     main()
