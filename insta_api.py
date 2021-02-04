ed210f4ed48e85fc5125fe79efa2015cbd5b72eb
Aleksandr Semenov <semenlinux@gmail.com>
2021-02-04 15:07:19 +0300

    Create function for download Hable Image by ID

--- insta_api.py
+++ insta_api.py
@@ -29,29 +29,44 @@ def download_image(link: str, filename: str, folder: str = "./") -> str:
 
 
 def fetch_spacex_last_launch(link_api: str) -> list:
-    folder = "./images"
+    image_folder = "./images"
     images_filepaths = []
     spacex_api_conten = get_data_from_link(link_api)
     spacex_image_links = spacex_api_conten.json().get("links").get("flickr_images")
-    for image_number, link in enumerate(spacex_image_links):
-        filename = f"spacex{ image_number }.jpg"
-        image_filepath = download_image(link, filename, folder)
+    for image_number, image_link in enumerate(spacex_image_links):
+        image_filename = f"spacex{ image_number }.jpg"
+        image_filepath = download_image(image_link, image_filename, image_folder)
         images_filepaths.append(image_filepath)
     return images_filepaths
 
 
-def fetch_hable_photo(link_api: str) -> list:
-    folder = "./images"
+def fetch_hable_photo(link_api: str, image_id: int) -> list:
+    image_folder = "./images_hable"
     images_filepaths = []
-    hable_api_conten = get_data_from_link(link_api)
-    hable_image_links = [f'https:{image_info.get("file_url")}' for image_info in hable_api_conten.json().get("image_files")]
-    return hable_image_links
+    hable_api_conten = get_data_from_link(f"{link_api}{image_id}")
+    hable_image_links = [
+        f'https:{image_info.get("file_url")}'
+        for image_info in hable_api_conten.json().get("image_files")
+    ]
+    for image_number, image_link in enumerate(hable_image_links):
+        image_extension = get_image_extension(image_link)
+        image_filename = f"hable{ image_number}.{ image_extension }"
+        image_filepath = download_image(image_link, image_filename, image_folder)
+        images_filepaths.append(image_filepath)
+    return images_filepaths
+
+
+def get_image_extension(link: str) -> str:
+    link_extension = link.split(".")[-1]
+    return link_extension
+
 
 def main():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     spacex_link_api = "https://api.spacexdata.com/v3/launches/67"
-    hable_link_api = "http://hubblesite.org//api/v3/image/1"
-    print(fetch_hable_photo(hable_link_api))
+    hable_link_api = "http://hubblesite.org//api/v3/image/"
+    hable_image_id = 1
+    print(fetch_hable_photo(hable_link_api, hable_image_id))
 
 
 if __name__ == "__main__":
