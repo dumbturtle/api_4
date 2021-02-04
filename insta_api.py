2e9164fbbdc3da1172503437bae6f3911a6b6888
Aleksandr Semenov <semenlinux@gmail.com>
2021-02-04 16:21:50 +0300

    Download photos from the Hubble collections

--- insta_api.py
+++ insta_api.py
@@ -28,10 +28,10 @@ def download_image(link: str, filename: str, folder: str = "./") -> str:
     return file_with_data_filepath
 
 
-def fetch_spacex_last_launch(link_api: str) -> list:
+def fetch_spacex_last_launch(image_link_api: str) -> list:
     image_folder = "./images"
     images_filepaths = []
-    spacex_api_conten = get_data_from_link(link_api)
+    spacex_api_conten = get_data_from_link(image_link_api)
     spacex_image_links = spacex_api_conten.json().get("links").get("flickr_images")
     for image_number, image_link in enumerate(spacex_image_links):
         image_filename = f"spacex{ image_number }.jpg"
@@ -40,20 +40,28 @@ def fetch_spacex_last_launch(link_api: str) -> list:
     return images_filepaths
 
 
-def fetch_hable_photo(link_api: str, image_id: int) -> list:
-    image_folder = "./images_hable"
+def fetch_hable_photo(image_link_api: str, image_id: int) -> list:
+    image_folder = "./images"
     images_filepaths = []
-    hable_api_conten = get_data_from_link(f"{link_api}{image_id}")
+    hable_api_content = get_data_from_link(f"{image_link_api}{image_id}")
     hable_image_links = [
         f'https:{image_info.get("file_url")}'
-        for image_info in hable_api_conten.json().get("image_files")
+        for image_info in hable_api_content.json().get("image_files")
     ]
+    print(hable_image_links)
     for image_number, image_link in enumerate(hable_image_links):
         image_extension = get_image_extension(image_link)
-        image_filename = f"hable{ image_number}.{ image_extension }"
+        image_filename = f"{ image_id }hable{ image_number}.{ image_extension }"
         image_filepath = download_image(image_link, image_filename, image_folder)
+        print(image_filepath)
         images_filepaths.append(image_filepath)
     return images_filepaths
+
+
+def fetch_hable_image_id(collection_link_api: str, collection_name: str) -> list:
+    hable_api_content = get_data_from_link(f"{collection_link_api}{collection_name}")
+    hable_images_id = [image_id.get("id") for image_id in hable_api_content.json()]
+    return hable_images_id
 
 
 def get_image_extension(link: str) -> str:
@@ -63,10 +71,15 @@ def get_image_extension(link: str) -> str:
 
 def main():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    spacex_link_api = "https://api.spacexdata.com/v3/launches/67"
-    hable_link_api = "http://hubblesite.org//api/v3/image/"
-    hable_image_id = 1
-    print(fetch_hable_photo(hable_link_api, hable_image_id))
+    spacex_image_link_api = "https://api.spacexdata.com/v3/launches/67"
+    hable_image_link_api = "http://hubblesite.org//api/v3/image/"
+    hable_collection_link_api = "http://hubblesite.org//api/v3/images/"
+    hable_collection_name = "stsci_gallery"
+    hable_image_range_id = fetch_hable_image_id(
+        hable_collection_link_api, hable_collection_name
+    )
+    for hable_image_id in hable_image_range_id:
+        fetch_hable_photo(hable_image_link_api, hable_image_id)
 
 
 if __name__ == "__main__":
