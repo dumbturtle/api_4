8041675e8cc43fe31a341d70fc23670e3c7316d9
Aleksandr Semenov <semenlinux@gmail.com>
2021-02-05 21:53:59 +0300

    Upload image to Instagram

---
+++ load_instagram.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+from instabot import Bot
+from PIL import Image
+
+load_dotenv()
+bot = Bot()
+
+
+def check_ratio(image_filepath: str) -> bool:
+    image = Image.open(image_filepath)
+    height, width = image.size
+    ratio = height / width
+    if ratio < 0.9:
+        return False
+    return True
+
+
+def main():
+    images_folder = os.environ["IMAGES_FOLDER"]
+    images = os.listdir(images_folder)
+    bot.login(username=os.environ["INSTAGRAM_USERNAME"], password=os.environ["INSTAGRAM_PASSWORD"])
+    for image in images:
+        string_filepath = os.path.join(images_folder, image)
+        if check_ratio(string_filepath):
+            bot.upload_photo(string_filepath, caption=string_filepath)
+            print(f"{ image }: { bot.api.last_response.status_code }")
+            continue
+        print(f"Неверное соотношение сторон: { image }")
+
+
+if __name__ == "__main__":
+    main()
