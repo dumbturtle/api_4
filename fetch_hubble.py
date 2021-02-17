3c28902b6d5d3e57499d2006f4a41f1f8cc184c3
Aleksandr Semenov <semenlinux@gmail.com>
2021-02-17 13:22:26 +0000

    Fix plural in variables

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -5,13 +5,8 @@ import requests
 from dotenv import load_dotenv
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-from handler import (
-    change_image_size_proportions,
-    convert_image_to_jpg,
-    download_image,
-    get_data_from_link,
-    get_image_extension,
-)
+from handler import (change_image_size_proportions, convert_image_to_jpg,
+                     download_image, get_data_from_link, get_image_extension)
 
 
 def fetch_hubble_photo(
@@ -26,16 +21,19 @@ def fetch_hubble_photo(
     for image_number, image_link in enumerate(hubble_image_links):
         image_extension = get_image_extension(image_link)
         image_filename = f"{ image_id }hubble{ image_number}.{ image_extension }"
-        image_filepath = download_image(image_link, image_filename, image_folder)
+        image_filepath = download_image(
+            image_link, image_filename, image_folder)
         change_image_size_proportions(image_filepath)
         image_jpg_filepath = convert_image_to_jpg(image_filepath)
         image_filepaths.append(image_jpg_filepath)
     return image_filepaths
 
 
-def fetch_hubble_image_id(collection_link_api: str, collection_name: str) -> list:
-    hubble_api_content = get_data_from_link(f"{collection_link_api}{collection_name}")
-    hubble_image_ids = [image_id.get("id") for image_id in hubble_api_content.json()]
+def fetch_hubble_image_ids(collection_link_api: str, collection_name: str) -> list:
+    hubble_api_content = get_data_from_link(
+        f"{collection_link_api}{collection_name}")
+    hubble_image_ids = [image_information.get(
+        "id") for image_information in hubble_api_content.json()]
     return hubble_image_ids
 
 
@@ -48,7 +46,7 @@ def main():
     image_folder = os.environ["IMAGE_FOLDER"]
     Path(f"./{ image_folder }").mkdir(parents=True, exist_ok=True)
     try:
-        hubble_image_ids = fetch_hubble_image_id(
+        hubble_image_ids = fetch_hubble_image_ids(
             hubble_collection_api_link, hubble_collection_name
         )
         for hubble_image_id in hubble_image_ids:
